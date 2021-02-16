4c76e6d36addb61a3408d751af1e591f6d11df2f
ZhukDima <zhuk@gmail.com>
2021-02-16 16:13:22 +0300

    Small fix

--- server.go
+++ server.go
@@ -12,28 +12,26 @@ func main() {
 	var tmplCheckin *template.Template
 	tmplLogin, err = template.ParseFiles("./src/login.html")
 	if err != nil {
-		log.Fatal("Unable to open html:", err)
+		log.Fatal("Unable to open html: ", err)
 	}
 	tmplCheckin, err = template.ParseFiles("./src/checkin.html")
 	if err != nil {
-		log.Fatal("Unable to open html:", err)
+		log.Fatal("Unable to open html: ", err)
 	}
 
-	fs := http.FileServer(http.Dir("./src"))
-	http.Handle("/src/", http.StripPrefix("/src/", fs))
+	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("./src"))))
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		if err = tmplLogin.Execute(rw, map[string]interface{}{}); err != nil {
-			log.Fatal("Unable to execute:", err)
+			log.Fatal("Unable to execute: ", err)
 		}
 	})
 	http.HandleFunc("/checkin", func(rw http.ResponseWriter, r *http.Request) {
 		if err = tmplCheckin.Execute(rw, map[string]interface{}{}); err != nil {
-			log.Fatal("Unable to execute:", err)
+			log.Fatal("Unable to execute: ", err)
 		}
 	})
-	// http.HandleFunc("/profile", http.FileServer(http.Dir("./src")))
 
 	if err = http.ListenAndServe(":3000", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
