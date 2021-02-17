5c5d13c88f9937eaa44f39b5f85e1cf69ab93a7b
ZhukDima <zhuk@gmail.com>
2021-02-17 20:56:27 +0300

    Фикс пары мелочей

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
-const host = "127.0.0.1"
-const port = 3000
+const host = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    req.url = "." + req.url
-    console.log("url", req.url);
+    req.url = '.' + req.url;
+    console.log('url', req.url);
     fs.readFile(req.url, (err, data) => {
         if (err) {
-            console.log("error", err);
+            console.log('error', err);
         } else {
             res.write(data);
         }
