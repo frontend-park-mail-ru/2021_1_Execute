307d5b9514eefa8396b26775b5164870343347da
MKondakova <milanon@ya.ru>
2021-02-21 13:36:27 +0300

    fix eslint and delete braces

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ const root = document.getElementById('root');
 const testItem = document.createElement('div');
 testItem.classList.add('menu-box');
 
-{
-  const testItemH1 = document.createElement('h1');
-  testItemH1.innerText = 'Тест';
+const testItemH1 = document.createElement('h1');
+testItemH1.innerText = 'Тест';
 
-  testItem.append(testItemH1);
-  testItem.append(testItemH1.cloneNode());
-  testItem.append(testItemH1.cloneNode(true));
-}
+testItem.append(testItemH1);
+testItem.append(testItemH1.cloneNode());
+testItem.append(testItemH1.cloneNode(true));
 
 root.append(testItem);
