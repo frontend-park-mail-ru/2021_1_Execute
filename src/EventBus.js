70b0a7428952f442ef2cc32a1a995e0a2b927a63
AlexisOMG <alesha-gramazeka@yandex.ru>
2021-02-21 21:27:06 +0300

    Added EventBus

---
+++ src/EventBus.js
@@ -0,0 +1,66 @@
+/**
+ * Класс представляет собой шину событий
+ * @class EventBus
+ */
+export default class EventBus {
+  constructor() {
+    this.events = new Map();
+  }
+
+  /**
+   * Функция проверяет имя события
+   * @param  {string} eventName - имя события
+   * @returns {boolean} - валидно или нет имя события
+   */
+  static validateEventName(eventName) {
+    return typeof eventName === 'string';
+  }
+
+  /**
+   * Метод позволяет назначить определенному событию функцию-обработчик
+   * @param  {string} eventName - имя события
+   * @param  {function} handler - функция-обработчик
+   * @throws ошибка, если передано невалидное имя события
+   * @throws ошибка, если переданный обработчик события - не функция
+   */
+  subscribe(eventName, handler) {
+    if (this.validateEventName(eventName)) {
+      throw new Error('Invalid event name');
+    }
+    if (typeof handler !== 'function') {
+      throw new Error('handler must be function');
+    }
+    this.events.set(eventName, handler);
+  }
+
+  /**
+   * Метод позволяет удалить у определенного события функцию-обработчик
+   * @param  {string} eventName - имя события
+   * @throws ошибка, если передано невалидное имя события
+   */
+  unsubscribe(eventName) {
+    if (this.validateEventName(eventName)) {
+      throw new Error('Invalid event name');
+    }
+    if (this.events.has(eventName)) {
+      this.events.delete(eventName);
+    }
+  }
+
+  /**
+   * Метод позволяет вызвать функцию-обработчик события с переданными данными
+   * @param  {string} eventName - имя события
+   * @param  {*} data - данные для функции-обработчика события
+   * @throws ошибка, если передано невалидное имя события
+   * @throws ошибка, если событию не назначен обработчик
+   */
+  call(eventName, data) {
+    if (this.validateEventName(eventName)) {
+      throw new Error('Invalid event name');
+    }
+    if (!this.events.has(eventName)) {
+      throw new Error('missing handler');
+    }
+    this.events.get(eventName)(data);
+  }
+}
