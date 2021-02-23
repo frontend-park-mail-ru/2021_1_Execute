84e6a2ae73f394249a5b58575cdac25964826501
AlexisOMG <alesha-gramazeka@yandex.ru>
2021-02-23 18:52:11 +0300

    Composition

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,6 +1,23 @@
 import EventBus from './EventBus.js';
 
-export default class Router extends EventBus {
+export default class Router {
+  constructor() {
+    this.EventBus = new EventBus();
+  }
+
+  subscribe(path, handler) {
+    this.EventBus.subscribe(path, handler);
+  }
+
+  unsubscribe(path) {
+    this.EventBus.unsubscribe(path);
+  }
+
+  go(path, ...data) {
+    this.addHistoryRecord(path);
+    this.EventBus.call(path, ...data);
+  }
+
   static addHistoryRecord(path, state = { urlPath: window.location.pathname }) {
     window.history.pushState(state, '', path);
   }
@@ -11,10 +28,5 @@ export default class Router extends EventBus {
 
   static forward() {
     window.history.forward();
-  }
-
-  go(path, ...data) {
-    this.addHistoryRecord(path);
-    this.call(path, data);
   }
 }
