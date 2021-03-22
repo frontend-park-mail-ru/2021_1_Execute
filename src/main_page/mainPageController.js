b495a3b2fcf807b1f78865cb50c91d379f74ad7f
AlexisOMG <alesha-gramazeka@yandex.ru>
2021-03-23 00:53:57 +0300

    Не знаю, как это назвать

--- src/main_page/mainPageController.js
+++ src/main_page/mainPageController.js
@@ -1,6 +1,50 @@
 import EventBus from '../utils/eventBus.js';
 import './main.handlebars.js';
 
+const context = {
+  authorized: true,
+  userDesks: [
+    {
+      id: 0,
+      name: '0',
+    },
+    {
+      id: 1,
+      name: '1',
+    },
+    {
+      id: 2,
+      name: '2',
+    },
+    {
+      id: 3,
+      name: '3',
+    },
+  ],
+  allDesks: [
+    {
+      id: 0,
+      name: '0',
+    },
+    {
+      id: 1,
+      name: '1',
+    },
+    {
+      id: 2,
+      name: '2',
+    },
+    {
+      id: 3,
+      name: '3',
+    },
+    {
+      id: 4,
+      name: '4',
+    },
+  ],
+};
+
 export default class MainPageController {
   constructor(router, root) {
     this.root = root;
@@ -8,6 +52,6 @@ export default class MainPageController {
   }
 
   start() {
-    this.root.innerHTML = Handlebars.templates.main();
+    this.root.innerHTML = Handlebars.templates.main(context);
   }
 }
